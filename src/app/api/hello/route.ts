6f79df2c1efef4ebb0599b6a6c72ab516850fd97
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-22 10:25:06 +0100

    working nextApi call to MongoDB

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,48 @@
+// import { NextResponse } from "next/server";
+
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   console.log(searchParams);
+//   // const id = searchParams.get('id');
+//   // const res = await fetch(`https://data.mongodb-api.com/product/${id}`, {
+//   //   headers: {
+//   //     'Content-Type': 'application/json'
+//   //   },
+//   // });
+//   // const product = await res.json();
+
+//   return NextResponse.json({ message: "hello" });
+// }
+
+
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse, NextRequest } from 'next/server'
+import * as mongoDB from 'mongodb'
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
-}
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name')
+
+  let db : mongoDB.Db;
+  let col: mongoDB.Collection;
+
+  const getProduct = async (name: string) => {
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@hackdayidea.bdc1jdl.mongodb.net/?retryWrites=true&w=majority`,
+    );
+    try {
+        await client.connect();
+        db = await client.db(`${process.env.MONGO_DB}`);
+        col = db.collection(`${process.env.MONGO_COLLECTION}`);
+        console.log('Connected successfully to server');
+        const data = (await col.find({ productName: `${name}`}).toArray());
+        return data[0];
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const data = await getProduct(name!)
+  console.log(data!.quantity)
+  return new NextResponse(data!.quantity)
+};
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import axios from 'axios'
 import Link from 'next/link'
 
-
-
 export default async function page() {
   const response = await axios.get(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/entries?access_token=${process.env.ACCESS_TOKEN}`)
   const data = response.data.items
